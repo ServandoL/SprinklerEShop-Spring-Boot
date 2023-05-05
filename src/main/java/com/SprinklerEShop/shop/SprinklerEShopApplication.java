e488b1c37f34a3aaf92c5cd15009042424aec524
servandoluviano <swyswy214@gmail.com>
2023-05-05 16:13:24 -0500

    init

---
+++ src/main/java/com/SprinklerEShop/shop/SprinklerEShopApplication.java
@@ -0,0 +1,13 @@
+package com.SprinklerEShop.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinklerEShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinklerEShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SprinklerEShop/shop/SprinklerEShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SprinklerEShop.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinklerEShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
